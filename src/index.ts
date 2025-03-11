1c57d48b6eed0bfe11f8e4d01905c639b6ff373e
elpulpo0 <chris.elpulpo@gmail.com>
2025-03-11 10:42:36 +0100

    feat: create new action: Get Address

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import createToken from "./actions/createToken";
 import swap from "./actions/swap";
 import receiveEgld from "./actions/receiveEgld";
 import lendEgld from "./actions/lendEgld";
+import getAddress from "./actions/getAddress";
 
 export const multiversxPlugin: Plugin = {
     name: "multiversx",
     description: "MultiversX Plugin for Eliza",
-    actions: [transfer, createToken, swap, receiveEgld, lendEgld],
+    actions: [transfer, createToken, swap, receiveEgld, lendEgld, getAddress],
     evaluators: [],
     providers: [],
 };
