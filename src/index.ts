7f4d7e49c14668224756de0a3e38600d875ddf82
elpulpo0 <chris.elpulpo@gmail.com>
2025-03-11 10:44:47 +0100

    feat: create new action: Check Wallet

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import swap from "./actions/swap";
 import receiveEgld from "./actions/receiveEgld";
 import lendEgld from "./actions/lendEgld";
 import getAddress from "./actions/getAddress";
+import checkWallet from "./actions/checkWallet";
 
 export const multiversxPlugin: Plugin = {
     name: "multiversx",
     description: "MultiversX Plugin for Eliza",
-    actions: [transfer, createToken, swap, receiveEgld, lendEgld, getAddress],
+    actions: [transfer, createToken, swap, receiveEgld, lendEgld, getAddress, checkWallet],
     evaluators: [],
     providers: [],
 };
