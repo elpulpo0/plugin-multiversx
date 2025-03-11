0e7ec73e2a71e153726abf672249b8a092a4a66e
elpulpo0 <chris.elpulpo@gmail.com>
2025-03-11 10:31:09 +0100

    feat: create new action: Receive EGLD

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import type { Plugin } from "@elizaos/core";
 import transfer from "./actions/transfer";
 import createToken from "./actions/createToken";
 import swap from "./actions/swap";
+import receiveEgld from "./actions/receiveEgld";
 
 export const multiversxPlugin: Plugin = {
     name: "multiversx",
     description: "MultiversX Plugin for Eliza",
-    actions: [transfer, createToken, swap],
+    actions: [transfer, createToken, swap, receiveEgld],
     evaluators: [],
     providers: [],
 };
