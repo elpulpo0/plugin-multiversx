252dbfe6b848227ab7c43364cebdacb0a28c1de9
elpulpo0 <chris.elpulpo@gmail.com>
2025-03-11 10:36:43 +0100

    feat: create new action: Lend EGLD

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import transfer from "./actions/transfer";
 import createToken from "./actions/createToken";
 import swap from "./actions/swap";
 import receiveEgld from "./actions/receiveEgld";
+import lendEgld from "./actions/lendEgld";
 
 export const multiversxPlugin: Plugin = {
     name: "multiversx",
     description: "MultiversX Plugin for Eliza",
-    actions: [transfer, createToken, swap, receiveEgld],
+    actions: [transfer, createToken, swap, receiveEgld, lendEgld],
     evaluators: [],
     providers: [],
 };
